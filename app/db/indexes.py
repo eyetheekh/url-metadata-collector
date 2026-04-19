c08066082363e7dbc97923549b7942678be101c5
eyetheekh <eyetheekhsp@gmail.com>
2026-04-19 21:51:52 +0530

    improve db indexes

--- app/db/indexes.py
+++ app/db/indexes.py
@@ -9,4 +9,12 @@ async def create_indexes(app: FastAPI, collection_name: str) -> None:
     await db[collection_name].create_index("url", unique=True)
 
     # TTL index for auto-cleanup (30 days)
-    await db[collection_name].create_index("process_state", expireAfterSeconds=2592000)
+    await db[collection_name].create_index("updated_at", expireAfterSeconds=2592000)
+
+    # querying of process state
+    await db[collection_name].create_index("process_state")
+
+    # querying of failed tasks
+    await db[collection_name].create_index(
+        [("process_state", 1), ("failure_count", 1), ("updated_at", 1)]
+    )
